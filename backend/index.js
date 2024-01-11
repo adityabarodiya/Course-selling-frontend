81aa1cc7c4a4cb7b87c33179a858425b83095acc
adityabarodiya <adi25kht@gmail.com>
2024-01-11 16:53:32 +0530

    Update

---
+++ backend/index.js
@@ -0,0 +1,230 @@
+/*## Create a course selling website
+
+### Description
+Functionally the same as 03-course-app-medium. Routes are the same as well.
+Rather than storing data in files, store them in MongoDB. 
+We will be covering this in the extra class next week but would be good for you to run ahead.
+
+## Routes
+### Admin Routes:
+ - POST /admin/signup
+   Description: Creates a new admin account.
+   Input: { username: 'admin', password: 'pass' }
+   Output: { message: 'Admin created successfully', token: 'jwt_token_here' }
+ - POST /admin/login
+   Description: Authenticates an admin. It requires the admin to send username and password in the headers.
+   Input: Headers: { 'username': 'admin', 'password': 'pass' }
+   Output: { message: 'Logged in successfully', token: 'jwt_token_here' }
+ - POST /admin/courses
+   Description: Creates a new course.
+   Input: Headers: { 'Authorization': 'Bearer jwt_token_here' }, Body: { title: 'course title', description: 'course description', price: 100, imageLink: 'https://linktoimage.com', published: true }
+   Output: { message: 'Course created successfully', courseId: 1 }
+ - PUT /admin/courses/:courseId
+   Description: Edits an existing course. courseId in the URL path should be replaced with the ID of the course to be edited.
+   Input: Headers: { 'Authorization': 'Bearer jwt_token_here' }, Body: { title: 'updated course title', description: 'updated course description', price: 100, imageLink: 'https://updatedlinktoimage.com', published: false }
+   Output: { message: 'Course updated successfully' }
+ - GET /admin/courses
+   Description: Returns all the courses.
+   Input: Headers: { 'Authorization': 'Bearer jwt_token_here' }
+   Output: { courses: [ { id: 1, title: 'course title', description: 'course description', price: 100, imageLink: 'https://linktoimage.com', published: true }, ... ] }
+   User Routes:
+
+### User routes
+ - POST /users/signup
+   Description: Creates a new user account.
+   Input: { username: 'user', password: 'pass' }
+   Output: { message: 'User created successfully', token: 'jwt_token_here' }
+ - POST /users/login
+   Description: Authenticates a user. It requires the user to send username and password in the headers.
+   Input: Headers: { 'username': 'user', 'password': 'pass' }
+   Output: { message: 'Logged in successfully', token: 'jwt_token_here' }
+ - GET /users/courses
+   Description: Lists all the courses.
+   Input: Headers: { 'Authorization': 'Bearer jwt_token_here' }
+   Output: { courses: [ { id: 1, title: 'course title', description: 'course description', price: 100, imageLink: 'https://linktoimage.com', published: true }, ... ] }
+ - POST /users/courses/:courseId
+   Description: Purchases a course. courseId in the URL path should be replaced with the ID of the course to be purchased.
+   Input: Headers: { 'Authorization': 'Bearer jwt_token_here' }
+   Output: { message: 'Course purchased successfully' }
+ - GET /users/purchasedCourses
+   Description: Lists all the courses purchased by the user.
+   Input: Headers: { 'Authorization': 'Bearer jwt_token_here' }
+   Output: { purchasedCourses: [ { id: 1, title: 'course title', description: 'course description', price: 100, imageLink: 'https://linktoimage.com', published: true }, ... ] }
+
+*/
+import express from "express";
+import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
+import cors from "cors"
+
+// const jwt = require("jsonwebtoken");
+// const mongoose = require("mongoose");
+const app = express();
+
+app.use(cors())
+
+app.use(express.json());
+
+const userSchema = new mongoose.Schema({
+  username: { type: String },
+  password: String,
+  purchasedCourses: [{ type: mongoose.Schema.Types.ObjectId, ref: "Course" }],
+});
+
+const adminSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+});
+
+const courseSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  price: Number
+});
+// Define mongoose models
+const User = mongoose.model("User", userSchema);
+const Admin = mongoose.model("Admin", adminSchema);
+const Course = mongoose.model("Course", courseSchema);
+
+mongoose.connect(
+  "mongodb+srv://adityabarodiya:xJgDIkvrklyd04Mt@cluster0.m6xjsds.mongodb.net/courses",
+  { useNewUrlParser: true, useUnifiedTopology: true, dbName: "courses" }
+);
+
+const SECRET = "Aditya";
+
+const authenticateJwt = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, SECRET, (err, user) => {
+      if (err) return res.sendStatus(403);
+      else {
+        req.user = user;
+        next();
+      }
+    });
+  } else {
+    res.sendStatus(401);
+  }
+};
+// Admin routes
+app.post("/admin/signup", async (req, res) => {
+  // logic to sign up admin
+  const { username, password } = req.body;
+
+  const admin = await Admin.findOne({ username });
+  if (admin) {
+    res.status(403).json({ massage: "Admin already exits" });
+  } else {
+    const newAdmin = new Admin({ username, password });
+    await newAdmin.save();
+    const token = jwt.sign({ username, role: "admin" }, SECRET, {
+      expiresIn: "1h",
+    });
+    res.json({ massage: "Admin Created succesfully", token });
+  }
+});
+
+app.post("/admin/login", async (req, res) => {
+  // logic to log in admin
+  const { username, password } = req.headers;
+  const admin = await Admin.findOne({ username, password });
+  if (admin) {
+    const token = jwt.sign({ username, role: "admin" }, SECRET, {
+      expiresIn: "1h",
+    });
+    res.json({ massage: "Admin login succesfully", token });
+  } else {
+    res.status(403).json({ massage: "Invalid username or password" });
+  }
+});
+
+app.post("/admin/courses", authenticateJwt, async (req, res) => {
+  // logic to create a course
+  const course = new Course(req.body);
+  await course.save();
+  res.json({ message: "Course created successfully", courseId: course.id });
+});
+
+app.put("/admin/courses/:courseId", authenticateJwt, async (req, res) => {
+  // logic to edit a course
+  const course = await Course.findByIdAndUpdate(req.params.courseId, req.body, {
+    new: true,
+  });
+  if (course) {
+    res.json({ message: "Course updated successfully" });
+  } else {
+    res.status(404).json({ message: "Course not found" });
+  }
+});
+
+app.get("/admin/courses", async (req, res) => {
+  // logic to get all courses
+  const courses = await Course.find({});
+  res.json({ courses });
+});
+
+// User routes
+app.post("/users/signup", async (req, res) => {
+  // logic to sign up user
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+
+  if (user) res.status(403).json({ massage: "User already exits !!!" });
+  else {
+    const newUser = new User({ username, password });
+    newUser.save();
+    const token = jwt.sign({ username, role: "user" }, SECRET, {
+      expiresIn: "1h",
+    });
+    res.json({ message: "User created successfully", token });
+  }
+});
+
+app.post("/users/login", async (req, res) => {
+  // logic to log in user
+  const { username, password } = req.headers;
+  const user = await User.find({ username, password });
+  if (user) {
+    const token = jwt.sign({ username, role: "user" }, SECRET, {
+      expiresIn: "1h",
+    });
+    res.json({ massage: "User login succesfully", token });
+  } else res.status(403).json({ massage: "Invalid username or password" });
+});
+
+app.get("/users/courses", authenticateJwt, async (req, res) => {
+  // logic to list all courses
+  const courses = await Course.find({ published: true });
+  res.json({ courses });
+});
+
+app.post("/users/courses/:courseId", authenticateJwt, async (req, res) => {
+  // logic to purchase a course
+  const course = await Course.findById(req.params.courseId);
+  if (course) {
+    const user = await User.find({ username: req.user.username });
+    if (user) {
+      user.purchasedCourses.push(course);
+      await user.save();
+      res.json({ massage: "course purchased succesfully" });
+    } else res.status(403).json({ message: "User not found" });
+  } else res.status(404).json({ message: "Course not found" });
+});
+
+app.get("/users/purchasedCourses", authenticateJwt, async (req, res) => {
+  // logic to view purchased courses
+  const user = await User.findOne({ username: req.user.username }).populate(
+    "purchasedCourses"
+  );
+  if (user) {
+    res.json({ purchasedCourses: user.purchasedCourses || [] });
+  } else {
+    res.status(403).json({ message: "User not found" });
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server is listening on port 3000");
+});
